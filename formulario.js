9b39fe72003cb58b4e81a76903426258017ba1e9
Proyectos <jossybr94@gmail.com>
2022-07-03 09:34:44 -0500

    Creando formulario con validación en JS

---
+++ formulario.js
@@ -0,0 +1,7 @@
+//(function(){
+    var formulario = document.getElementById('formulario'),
+        nombre = formulario.nombre,
+        correo = formulario.correo,
+        sexo = formulario.sexo,
+        terminos = formulario.terminos
+//}())
